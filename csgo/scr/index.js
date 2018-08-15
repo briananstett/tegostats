588b3b3be88078ab5cbd2f775b6971e509934f14
Brian Anstett <brian.anstett@24g.com>
2018-08-15 12:51:50 -0400

    accuracy, headshots, winrate, killdeath

--- csgo/scr/index.js
+++ csgo/scr/index.js
@@ -16,6 +16,84 @@ const gameID = configuration.gameID;
 const apiKey = configuration.steamKey;
 const concurrency = configuration.concurrency;
 
+//support functions
+/**
+ * Head function that handles all parsing of stat data
+ * @param {object} rawStats Stringified verstion CSGO stats
+ * @return {promise}
+ */
+function parser(rawStats){
+    return new Promise((resolve, reject)=>{
+        let savedStats = {};
+        var stats = JSON.parse(rawStats).playerstats.stats;
+        
+
+        accuracy(stats, savedStats);
+        headShots(stats, savedStats);
+        winRate(stats, savedStats);
+        killDeath(stats,savedStats)
+        return resolve(savedStats);
+        //TODO(Developer) Please find a better way to do this
+    })
+
+}
+
+/**
+ * Shot accuracy
+ * @param {object} stats JSON object of raw stats.
+ * @param {object} save JSON object to pusha saved status
+ */
+function accuracy(stats, save){
+    let shotsFired = stats.total_shots_fired.value;
+    let shotsHit = stats.total_shots_hit.value;
+
+    let accuracy = Math.floor((shotsHit / shotsFired) *100 );
+    
+    save.accuracyPercent = accuracy;
+}
+
+/**
+ * Percentage of headshots
+ * @param {object} stats JSON object of raw stats.
+ * @param {object} save JSON object to pusha saved status
+ */
+function headShots(stats, save){
+    let shotshit = stats.total_shots_hit.value;
+    let headshots = stats.total_kills_headshot.value;
+
+    let headshotPercent = Math.floor((headshots/shotshit) * 100);
+    
+    save.headshotPercent = headshotPercent;
+}
+
+/**
+ * win rate
+ * @param {object} stats JSON object of raw stats.
+ * @param {object} save JSON object to pusha saved status
+ * @description Calculates the win and loose rate of the player
+ */
+function winRate(stats, save){
+    let wins = stats.total_wins.value;
+    let totalRounds = stats.total_rounds_played.value;
+
+    let winRate = Math.floor((wins/totalRounds) * 100);
+    save.winRate = winRate;
+}
+
+/**
+ * Kill/Death ratio
+ * @param {object} stats JSON object of raw stats.
+ * @param {object} save JSON object to pusha saved status
+ * @description Calculates the Kill Death ratio
+ */
+function killDeath(stats, save){
+    let kills = stats.total_kills.value;
+    let deaths = stats.total_deaths.value;
+
+    let kdRatio = Math.floor((kills/deaths) * 100);
+    save.kdRatio = kdRatio;
+}
+
 
 queue.process('csgo', concurrency, function(message, done){
     let steamID = message.data.steamID;
@@ -24,12 +102,18 @@ queue.process('csgo', concurrency, function(message, done){
     rp({
         uri: requestURI
     }).then((csgoStats=>{
-        console.log(csgoStats)
-        done();
-        //TODO(Developer) process data and add to cloudStore
+        // console.log(csgoStats)
+        parser(csgoStats)
+            .then(savedStates=>{
+                //update cloud store
+                console.log(savedStates);
+                done();
+            }).catch(error=>{
+                done(error);
+            });
     })).catch(error=>{
         //Request failed
-        console.log(error.response.statusCode);
+        console.log('DEBUG',error);
         let requestError = new Error("Bad Request")
             requestError.URI = requestURI;
             requestError.userID = userID;
@@ -37,5 +121,8 @@ queue.process('csgo', concurrency, function(message, done){
     });
     
 });
+
+
+
 
 
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var kue= require('kue'),
 
 
 var job = que.create('csgo',{
-    steamID: "76561198037475921",
-    userID: '4'
+    // steamID: "76561198037475921",
+    steamID: '76561198099273779',
+    userID: '881a812d-b23a-4cb7-9e5e-c1873da6c1f9'
 }).removeOnComplete(true).save(error=>{
     if(error) console.log(error);
     console.log(job.id);
 })   
-
 
 kue.app.listen(3000);
